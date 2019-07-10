137c7d49b9348ee75a1653ed965b02ba3379db80
t-tullis <t.tullis34@gmail.com>
2019-07-10 11:03:03 -0700

    Added code to gp blockchain.py file

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -12,7 +12,8 @@ class Blockchain(object):
         self.current_transactions = []
         self.nodes = set()
 
-        self.new_block(previous_hash=1, proof=100)
+        #creates the genesis block
+        self.new_block(previous_hash=1, proof=99) 
 
     def new_block(self, proof, previous_hash=None):
         """
@@ -82,7 +83,11 @@ class Blockchain(object):
         - p is the previous proof, and p' is the new proof
         """
 
-        pass
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
 
     @staticmethod
     def valid_proof(last_proof, proof):
@@ -91,7 +96,10 @@ class Blockchain(object):
         leading zeroes?
         """
         # TODO
-        pass
+        guess = f"{last_proof}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:4] == '0000'
 
     def valid_chain(self, chain):
         """
@@ -140,12 +148,14 @@ def mine():
 
     # We must receive a reward for finding the proof.
     # TODO:
+    blockchain.new_transaction(0, node_identifier, 1)
     # The sender is "0" to signify that this node has mine a new coin
     # The recipient is the current node, it did the mining!
     # The amount is 1 coin as a reward for mining the next block
 
     # Forge the new Block by adding it to the chain
     # TODO
+    block = blockchain.new_block(proof, blockchain.hash(last_block))
 
     # Send a response with the new block
     response = {
@@ -180,6 +190,8 @@ def new_transaction():
 def full_chain():
     response = {
         # TODO: Return the chain and its current length
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
     }
     return jsonify(response), 200
 
